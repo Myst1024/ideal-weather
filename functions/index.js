fc75b41d6de8a3f7824c5c050991fcc583824c02
Gunnar Carlson <gunnar.carlson@gmail.com>
2020-07-31 14:40:01 -0500

    fix: getForecase error handling

--- functions/index.js
+++ functions/index.js
@@ -12,11 +12,8 @@ const OPENWEATHER_3HR_URL = "https://api.openweathermap.org/data/2.5/forecast";
 /** PUBLIC */
 exports.getForecast = functions.https.onRequest((req, res) => {
     if (!req.query.zip) {
-        throw new functions.https.HttpsError(
-            "invalid-argument",
-            "The function must be called with " +
-            'one arguments "zip" containing the zip code to query.'
-        );
+        return res.status(422).send('The function must be called with ' +
+        'one argument "zip" containing the zip code to query.')
     }
     const zip = req.query.zip.toString();
 
@@ -24,10 +21,10 @@ exports.getForecast = functions.https.onRequest((req, res) => {
         .then((forecast) => {
             return res.json(forecast);
         })
-        .catch((err) => {
-            throw err;
+        .catch(() => {
+            return res.status(503).send('Error from Openweather API');
         });
-    functions.logger.info("ran");
+    functions.logger.info("ran getForecast");
 });
 
 /** PRIVATE */
