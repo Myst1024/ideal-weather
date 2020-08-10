d426c80cd7d789823f4f8179e37757b9d8a28381
Gunnar Carlson <gunnar.carlson@gmail.com>
2020-08-09 21:57:05 -0500

    feat: forecast hover tooltips fix: wind converted to MPH

--- src/components/forecast/Day.js
+++ src/components/forecast/Day.js
@@ -1,4 +1,9 @@
 import React from "react";
+import Tooltip from "@material-ui/core/Tooltip";
+import Zoom from "@material-ui/core/Zoom";
+
+const WEIGHTED_TIER_RATIO = 0.25; // Ratio defining % of preference range to weight less
+const WEIGHTED_TIER_MULTIPLIER = 0.6; // Amount to reduce negative weight if within ratio
 
 function Day({ name, forecasts, preferences }) {
   const forecastElements = forecasts.map((forecast, i) => {
@@ -7,16 +12,30 @@ function Day({ name, forecasts, preferences }) {
       hour12: true,
     });
 
-    return (
-      <div className="day-forecasts-segment" key={i}>
-        <div
-          className="bar"
-          style={{
-            height: calculateForecastBarHeight(preferences, forecast) + "%",
-          }}
-        ></div>
-        <div className="time">{time}</div>
+    const HoverForecast = (
+      <div className="forecast-tooltip">
+        <div>
+          {`Temp: ${Math.round(kelvinToFahrenheit(forecast.main.temp))}°F`}{" "}
+        </div>
+        <div> {`Humidity: ${forecast.main.humidity}%`} </div>
+        <div> {`Wind: ${Math.round(msToMph(forecast.wind.speed))} MPH`} </div>
+        <div> {`Rain: ${forecast.rain ? forecast.rain["3h"] : 0}%`}</div>
       </div>
+    );
+
+    return (
+      <Tooltip key={i} TransitionComponent={Zoom} arrow title={HoverForecast}>
+        <div className="day-forecasts-segment">
+          <div
+            className="bar"
+            style={{
+              height: calculateForecastBarHeight(preferences, forecast) + "%",
+            }}
+          ></div>
+
+          <div className="time">{time}</div>
+        </div>
+      </Tooltip>
     );
   });
 
@@ -34,68 +53,51 @@ function Day({ name, forecasts, preferences }) {
  * @param {Object} forecast
  */
 function calculateForecastBarHeight(preferences, forecast) {
-  const weights = {temp: 75, humidity: 55, wind: 40, rain: 100};
+  const weights = { temp: 80, humidity: 60, wind: 50, rain: 100 };
   let negativesSum = 0;
 
-
   // Temperature
-  const kelvinRange = preferences.userTemperature.map(temp => fahrenheitToKelvin(temp));
-  if (
-    !isInRange(kelvinRange,forecast.main.temp)
-  ) {
+  const kelvinRange = preferences.userTemperature.map((temp) =>
+    fahrenheitToKelvin(temp)
+  );
+  if (!isInRange(kelvinRange, forecast.main.temp)) {
     negativesSum += weights.temp;
+  } else if (isInTieredRange(kelvinRange, forecast.main.temp)) {
+    negativesSum += weights.temp * WEIGHTED_TIER_MULTIPLIER;
   }
 
   // Humidity
   if (!isInRange(preferences.userHumidity, forecast.main.humidity)) {
     negativesSum += weights.humidity;
+  } else if (
+    isInTieredRange(preferences.userHumidity, forecast.main.humidity)
+  ) {
+    negativesSum += weights.humidity * WEIGHTED_TIER_MULTIPLIER;
   }
 
   // Wind
-  if (!isInRange(preferences.userWind, forecast.wind.speed)) {
+  if (!isInRange(preferences.userWind, msToMph(forecast.wind.speed))) {
     negativesSum += weights.wind;
+  } else if (
+    isInTieredRange(preferences.userWind, msToMph(forecast.wind.speed))
+  ) {
+    negativesSum += weights.wind * WEIGHTED_TIER_MULTIPLIER;
   }
 
   // Rain
   // OpenWeatherApi excludes the rain key if none is expected
-  if (!forecast.rain) {
-    negativesSum += 0
-  } else if (!isInRange(preferences.userRain, forecast.rain["3h"])) {
-        negativesSum +=  weights.rain;
+  if (forecast.rain) {
+    if (!isInRange(preferences.userRain, forecast.rain["3h"])) {
+      negativesSum += weights.rain;
+    } else if (isInTieredRange(preferences.userRain, forecast.rain["3h"])) {
+      console.log("weighted rain");
+      negativesSum += weights.rain * WEIGHTED_TIER_MULTIPLIER;
+    }
   }
 
-  const height = Math.max(0,100 - negativesSum)
+  const height = Math.max(0, 100 - negativesSum);
   return height;
 }
-
-/**
- * 
- * @param {Array[Number]} range 
- * @param {Number} value 
- * @param {Number} weight 
- */
-function getWeightedHeight(range, value, weight) {
-  const rangeMean = (range[0] + range[1]) / 2
-
-  if (value <= rangeMean) {
-      return 0
-  } else {
-      // calculate ratio between max range and value
-      return weight * ((range[1] - value + 0 ) / (rangeMean - range[0]))
-  }
-}
-
-function getWeightedNegatives(range, value, weight) {
-  const rangeMean = (range[0] + range[1]) / 2
-  if (value <= rangeMean) {
-      return 0
-  } else {
-      // calculate ratio between max range and value
-      return weight - weight * ((range[1] - value + 0 ) / (rangeMean - range[0]))
-  }
-}
-
-
 
 /**
  * Convert from Kelvin to Fahrenheit
@@ -106,12 +108,47 @@ function fahrenheitToKelvin(temp) {
 }
 
 /**
+ * Convert from Fahrenheit to Kelvin
+ * @param {Number} temp
+ */
+function kelvinToFahrenheit(temp) {
+  return ((temp - 273.15) * 9) / 5 + 32;
+}
+
+/**
+ * Convert from M/S to MPH
+ * @param {Number} temp
+ */
+function msToMph(speed) {
+  return speed * 2.237;
+}
+
+/**
  * Returns True if value is between first and second values of given range
  * @param {Array[Number, Number]} range
  * @param {Number} value
  */
 function isInRange(range, value) {
   return value >= range[0] && value <= range[1];
+}
+
+/**
+ * Returns True if value is between first and second values of given range
+ * modified by WEIGHTED_TIER_RATIO
+ * @param {Array[Number, Number]} range
+ * @param {Number} value
+ */
+function isInTieredRange(range, value) {
+  const difference = range[1] - range[0];
+  const weightedDifference = difference * WEIGHTED_TIER_RATIO;
+  // if min value of range is 0, assume that is ideal and not the minimum bearable
+  if (range[0] === 0) {
+    return value > range[1] - weightedDifference;
+  }
+  return (
+    value < range[0] + weightedDifference ||
+    value > range[1] - weightedDifference
+  );
 }
 
 export default Day;
