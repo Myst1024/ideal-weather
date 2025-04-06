dd8f0510d4cfb88997a834bc04bb9acbe8f1932d
Myst1024 <gunnar.carlson@gmail.com>
2025-04-06 18:01:26 -0500

    fix: upgrade to firebase functions v2

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,47 @@
-const functions = require("firebase-functions");
+const functions = require("firebase-functions/v2");
+const { defineSecret } = require("firebase-functions/params");
 const axios = require("axios");
-//const admin = require("firebase-admin");
 
 /**
  * CONSTANTS
  */
-const ENVIRONMENT = functions.config();
-const OPENWEATHER_API_KEY = ENVIRONMENT["ideal-weather"].key;
+const OPENWEATHER_API_KEY = defineSecret("OPENWEATHER_API_KEY");
 const OPENWEATHER_3HR_URL = "https://api.openweathermap.org/data/2.5/forecast";
 
 /** PUBLIC */
-exports.getForecast = functions.https.onRequest((req, res) => {
-  functions.logger.info("ran getForecast");
-  setHeaders(req, res);
-  if (!req.query.zip) {
-    return res
-      .status(422)
-      .send(
-        "The function must be called with " +
-          'one argument "zip" containing the zip code to query.'
-      );
-  }
-  const zip = req.query.zip.toString();
+exports.getForecastv2 = functions.https.onRequest(
+  { secrets: [OPENWEATHER_API_KEY] },
+  async (req, res) => {
+    functions.logger.info("ran getForecast");
+    setHeaders(req, res);
+    if (!req.query.zip) {
+      return res
+        .status(422)
+        .send(
+          "The function must be called with " +
+            'one argument "zip" containing the zip code to query.'
+        );
+    }
+    const zip = req.query.zip.toString();
 
-  return callOpenWeather(zip)
-    .then((forecast) => {
-      return res.json(forecast);
-    })
-    .catch(() => {
-      return res.status(503).send("Error from Openweather API");
-    });
-});
+    return callOpenWeather(zip)
+      .then((forecast) => {
+        return res.json(forecast);
+      })
+      .catch(() => {
+        return res.status(503).send("Error from Openweather API");
+      });
+  }
+);
 
 /** PRIVATE */
 
 const setHeaders = function (req, res) {
-  var whitelist = ["http://localhost:3000", "https://ideal-weather.web.app"];
+  var whitelist = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://ideal-weather.web.app",
+  ];
   var origin = req.headers.origin;
   if (whitelist.indexOf(origin) > -1) {
     res.setHeader("Access-Control-Allow-Origin", origin);
@@ -47,7 +53,7 @@ const setHeaders = function (req, res) {
 
 const callOpenWeather = async (zip) => {
   const params = {
-    params: { zip: `${zip},us`, appid: OPENWEATHER_API_KEY },
+    params: { zip: `${zip},us`, appid: OPENWEATHER_API_KEY.value() },
   };
 
   return axios
